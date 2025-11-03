20dd529dc6e18a7852d1e5811f78107113ecce24
ritvik7 <ritvikgaur7@gmail.com>
2025-11-03 08:45:33 -0800

    wrapping, tmux wip prefix highlighting

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Enable word wrapping for org files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "org",
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true        -- Break at word boundaries, not mid-word
+    vim.opt_local.breakindent = true      -- Indent wrapped lines to match indentation
+  end,
+})
--- nvim/lua/plugins/orgmode.lua
+++ nvim/lua/plugins/orgmode.lua
@@ -8,14 +8,51 @@ return {
       require("orgmode").setup({
         org_agenda_files = "~/Documents/zorg/**/*",
         org_default_notes_file = "~/Documents/zorg/refile.org",
+        org_highlight_latex_and_related = "entities",
+        mappings = {
+          org_return_uses_meta_return = true,
+          org = {
+            org_toggle_checkbox = "<C-y>",
+          },
+        },
+        ui = {
+          fold = {
+            enable = true,
+          },
+        },
+        -- Performance optimizations
+        org_startup_folded = "showeverything", -- Don't fold on startup (faster)
+        org_log_done = false, -- Disable logging for performance
       })
 
-      -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
-      -- add ~org~ to ignore_install
-      -- require('nvim-treesitter.configs').setup({
-      --   ensure_installed = 'all',
-      --   ignore_install = { 'org' },
-      -- })
+      -- Set up proper highlights for org-mode after colorscheme loads
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        pattern = "*",
+        callback = function()
+          -- Get Catppuccin colors
+          local colors = require("catppuccin.palettes").get_palette()
+
+          -- Define org-specific highlights
+          vim.api.nvim_set_hl(0, "@org.headline.level1", { fg = colors.blue, bold = true })
+          vim.api.nvim_set_hl(0, "@org.headline.level2", { fg = colors.sapphire, bold = true })
+          vim.api.nvim_set_hl(0, "@org.headline.level3", { fg = colors.sky, bold = true })
+          vim.api.nvim_set_hl(0, "@org.headline.level4", { fg = colors.teal, bold = true })
+          vim.api.nvim_set_hl(0, "@org.headline.level5", { fg = colors.lavender, bold = true })
+          vim.api.nvim_set_hl(0, "@org.headline.level6", { fg = colors.mauve, bold = true })
+
+          vim.api.nvim_set_hl(0, "OrgTODO", { fg = colors.red, bold = true })
+          vim.api.nvim_set_hl(0, "OrgDONE", { fg = colors.green, bold = true })
+          vim.api.nvim_set_hl(0, "@org.keyword.todo", { fg = colors.red, bold = true })
+          vim.api.nvim_set_hl(0, "@org.keyword.done", { fg = colors.green, bold = true })
+
+          vim.api.nvim_set_hl(0, "Headline", { bg = colors.surface0 })
+          vim.api.nvim_set_hl(0, "CodeBlock", { bg = colors.mantle })
+          vim.api.nvim_set_hl(0, "Dash", { fg = colors.overlay0 })
+        end,
+      })
+
+      -- Apply highlights immediately
+      vim.cmd("doautocmd ColorScheme")
     end,
   },
   {
@@ -32,36 +69,15 @@ return {
     event = "VeryLazy",
     ft = { "org" },
     config = function()
-      require("org-bullets").setup({
-        concealcursor = false,
-        symbols = {
-          headlines = { "◉", "○", "✸", "✿" },
-          checkboxes = {
-            half = { "◐", "@org.checkbox.halfchecked" },
-            done = { "✓", "@org.keyword.done" },
-            todo = { "◯", "@org.keyword.todo" },
-          },
-        },
-      })
+      require("org-bullets").setup()
     end,
   },
   {
     "lukas-reineke/headlines.nvim",
     event = "VeryLazy",
     ft = { "org" },
-    config = function()
-      require("headlines").setup({
-        org = {
-          headline_highlights = { "Headline1", "Headline2", "Headline3", "Headline4", "Headline5", "Headline6" },
-          codeblock_highlight = "CodeBlock",
-          dash_highlight = "Dash",
-          quote_highlight = "Quote",
-          fat_headlines = true,
-          fat_headline_upper_string = "▃",
-          fat_headline_lower_string = "▂",
-        },
-      })
-    end,
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = true,
   },
   {
     "hamidi-dev/org-super-agenda.nvim",
@@ -69,7 +85,79 @@ return {
     ft = { "org" },
     config = function()
       require("org-super-agenda").setup({
-        -- Default settings
+        org_super_agenda_groups = {
+          -- Urgent/Overdue (Red)
+          {
+            name = "   Overdue",
+            overdue = true,
+          },
+          {
+            name = "   Due Today",
+            due = "today",
+          },
+
+          -- Critical Priority (Red/Urgent)
+          {
+            name = "   Critical [#A]",
+            priority = "A",
+          },
+
+          -- Important Priority (Yellow/Important)
+          {
+            name = "   Important [#B]",
+            priority = "B",
+          },
+
+          -- Work This Week (Blue)
+          {
+            name = "   Work (This Week)",
+            file = "work.org",
+            due = "week",
+          },
+
+          -- Life This Week (Green)
+          {
+            name = "   Life (This Week)",
+            file = "life.org",
+            due = "week",
+          },
+
+          -- Health Tasks (Purple)
+          {
+            name = "   Health",
+            tag = "health",
+          },
+
+          -- Finance Tasks (Cyan)
+          {
+            name = "   Finance",
+            tag = "finance",
+          },
+
+          -- Learning/Development (Magenta)
+          {
+            name = "   Learning",
+            tag = "learning",
+          },
+
+          -- Hobbies/Personal (Pink)
+          {
+            name = "   Hobbies",
+            tag = "hobbies",
+          },
+
+          -- Someday Tasks (Gray)
+          {
+            name = "   Someday",
+            tag = "someday",
+          },
+
+          -- Catch-all for everything else
+          {
+            name = "   Everything Else",
+            any = {},
+          },
+        },
       })
     end,
   },
