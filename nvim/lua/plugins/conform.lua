f27651d285daf8d37f49ca696906a378381401c0
ritvik7 <ritvikgaur7@gmail.com>
2025-12-20 23:01:40 -0800

    org mode formatting

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -2,7 +2,42 @@ return {
     "stevearc/conform.nvim",
     opts = {
         formatters_by_ft = {
-            org = { "trim_whitespace", "trim_newlines" },
+            org = { "org_space_todos", "trim_whitespace", "trim_newlines" },
+        },
+        formatters = {
+            org_space_todos = {
+                meta = {
+                    url = "internal",
+                    description = "Add spacing between top-level org todos",
+                },
+                format = function(self, ctx, lines, callback)
+                    local result = {}
+
+                    for i = 1, #lines do
+                        local line = lines[i]
+
+                        -- Remove leading spaces from org headings
+                        -- e.g., "  * TODO item" becomes "* TODO item"
+                        local rest = line:match("^%s*(%*.*)")
+                        if rest then
+                            line = rest
+                        end
+
+                        table.insert(result, line)
+
+                        local next_line = lines[i + 1]
+                        local current_is_blank = line:match("^%s*$") ~= nil
+                        local next_is_top_level = next_line and next_line:match("^%*%s") ~= nil
+
+                        -- Add blank line before a top-level item if current line is not blank
+                        if not current_is_blank and next_is_top_level then
+                            table.insert(result, "")
+                        end
+                    end
+
+                    callback(nil, result)
+                end,
+            },
         },
     },
 }
