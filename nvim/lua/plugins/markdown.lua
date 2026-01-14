440672a2b2732f80e3ff68f79d9f349230596b78
ritvik7 <ritvikgaur7@gmail.com>
2026-01-13 22:48:53 -0800

    markdown links

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -10,11 +10,36 @@ return {
     ft = "markdown",
   },
   {
-    "tpope/vim-markdown",
+    "preservim/vim-markdown",
     ft = "markdown",
-    config = function()
+    init = function()
       vim.g.vim_markdown_follow_link = 1
-      vim.g.vim_markdown_edit_link_in = "current_window"
+      vim.g.vim_markdown_edit_url_in = "current_window"
+
+      local function open_markdown_link()
+        local line = vim.api.nvim_get_current_line()
+        local col = vim.api.nvim_win_get_cursor(0)[2] + 1
+
+        local url = line:match('%[.*%]%(([^)]+)%)')
+        if not url then
+          url = line:match('<([^>]+)>')
+        end
+
+        if not url then
+          return
+        end
+
+        if url:match('^http') then
+          vim.fn.system('open ' .. url)
+        else
+          local current_dir = vim.fn.expand('%:p:h')
+          local full_path = current_dir .. '/' .. url
+          vim.api.nvim_command('edit ' .. vim.fn.fnameescape(full_path))
+        end
+      end
+
+      vim.keymap.set({ "n", "v" }, "gL", open_markdown_link)
+      vim.keymap.set({ "n", "v" }, "gW", "<Plug>Markdown_OpenUrlUnderCursor")
     end,
   },
 }
