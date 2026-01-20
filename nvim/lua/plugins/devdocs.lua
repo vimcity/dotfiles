252b9ac3c1e1815373f2403e131eefc43890b95b
vimcity <ritvikgaur7@gmail.com>
2026-01-20 10:59:30 -0800

    devdocs, mvn alias

---
+++ nvim/lua/plugins/devdocs.lua
@@ -0,0 +1,33 @@
+return {
+  "luckasRanarison/nvim-devdocs",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  opts = {
+    after_open = function(bufnr)
+      local keymap_opts = { noremap = true, silent = true, buffer = bufnr }
+      -- Scroll down with 'j' or 'd'
+      vim.keymap.set("n", "j", "<C-d>", keymap_opts)
+      vim.keymap.set("n", "d", "<C-d>", keymap_opts)
+      -- Scroll up with 'k' or 'u'
+      vim.keymap.set("n", "k", "<C-u>", keymap_opts)
+      vim.keymap.set("n", "u", "<C-u>", keymap_opts)
+      -- Close with 'q' or 'Esc'
+      vim.keymap.set("n", "q", ":close<CR>", keymap_opts)
+      vim.keymap.set("n", "<Esc>", ":close<CR>", keymap_opts)
+      
+      -- Auto-focus the floating window after it renders
+      vim.schedule(function()
+        local win_id = vim.fn.bufwinid(bufnr)
+        if win_id ~= -1 then
+          vim.api.nvim_set_current_win(win_id)
+        end
+      end)
+    end,
+  },
+  config = function(_, opts)
+    require("nvim-devdocs").setup(opts)
+  end,
+}
