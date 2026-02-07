c151abe3ef0d31c9dc84c6adfe32c2e2c5434efd
rgaur <ritvik.gaur@sony.com>
2026-02-06 17:25:18 -0800

    tmux theming icons, alignment

--- tmux/scripts/path.sh
+++ tmux/scripts/path.sh
@@ -21,10 +21,13 @@ if [ ${#display_path} -gt 40 ]; then
   display_path="...${display_path: -37}"
 fi
 
+# Folder nerd icon
+icon=""
+
 # Catppuccin colors - using purple/pink for directory
 bg_color="#c6a0f6"  # Catppuccin Frappe Lavender (purple/pink)
 text_dark="#000000" # Dark text for contrast
 
 # Output with TMUX color codes for pill box styling
 # Format: [color code] space icon path space [reset]
-printf "#[bg=%s,fg=%s,bold]  %s #[default]" "$bg_color" "$text_dark" "$display_path"
+printf "#[bg=%s,fg=%s,bold] %s %s #[default]" "$bg_color" "$text_dark" "$icon" "$display_path"
