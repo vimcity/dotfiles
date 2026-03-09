8638c34f8c74b0d6b003d05bdc24bb94153513c1
ritvik7 <ritvikgaur7@gmail.com>
2026-03-09 01:04:14 -0700

    dap ui console, types with lazydev

--- nvim/lua/plugins/dap-ui.lua
+++ nvim/lua/plugins/dap-ui.lua
@@ -1,9 +1,29 @@
 return {
   {
     "rcarriga/nvim-dap-ui",
-    opts = {
-      force_close = false, -- Keeps the DAP UI open after debug ends
-    },
+    opts = function(_, opts)
+      opts.force_close = false -- Keep DAP UI open after debug ends
+
+      if opts.layouts and opts.layouts[2] and opts.layouts[2].position == "bottom" then
+        opts.layouts[2].elements = {
+          { id = "repl", size = 1.0 },
+        }
+      end
+
+      local dap = require("dap")
+      local dapui = require("dapui")
+
+      dap.listeners.after.event_initialized["dapui_console_float"] = function()
+        vim.schedule(function()
+          dapui.float_element("console", {
+            border = "single",
+            enter = false,
+            width = math.floor(vim.o.columns * 0.7),
+            height = math.floor(vim.o.lines * 0.35),
+          })
+        end)
+      end
+    end,
   },
   {
     "theHamsta/nvim-dap-virtual-text",
