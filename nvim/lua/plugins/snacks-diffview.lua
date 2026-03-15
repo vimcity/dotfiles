1d73e28aa4437511e739388e772f0af4961fc76f
vimcity <ritvikgaur7@gmail.com>
2026-03-15 12:18:45 -0700

    clipboard ssh, diffview

--- nvim/lua/plugins/snacks-diffview.lua
+++ nvim/lua/plugins/snacks-diffview.lua
@@ -2,7 +2,7 @@
 -- Bridge plugin combining Snacks.gh PR workflow with DiffView.nvim layout
 
 return {
-  dir = "$HOME/Projects/snacks-diffview-integration/snacks-diffview",
+  dir = "~/dotfiles/snacks-diffview",
   name = "snacks-diffview",
   dependencies = {
     "folke/snacks.nvim",
