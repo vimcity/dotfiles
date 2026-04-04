9496ac37f2dfe3b4529e717c873d083d8cb021bb
vimcity <ritvikgaur7@gmail.com>
2026-04-04 15:05:13 -0700

    Update directory color to lighter rose pink (#d08cbe) for prettier Rose Pine theme

--- prompt-themes.zsh
+++ prompt-themes.zsh
@@ -17,8 +17,8 @@ define_rose_pine_theme() {
         # Main segments - Rose Pine Moon (darker variant)
         [user_bg]="#323238"
         [user_fg]="#ebbcba"
-        [dir_bg]="#6e3e7f"
-        [dir_fg]="#f0e9f6"
+        [dir_bg]="#d08cbe"
+        [dir_fg]="#191724"
         [git_bg]="#2a283e"
         [git_fg]="#ebbcba"
         [venv_bg]="#323238"
