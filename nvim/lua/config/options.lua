ae1338d17b1b2de0d23c1949f3ae4dbd69c25c68
vimcity <ritvikgaur7@gmail.com>
2026-04-05 02:24:47 -0700

    fast python

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.loaded_python3_provider = 0
+
 -- Tab and indentation settings
 vim.opt.tabstop = 4 -- Number of spaces that a <Tab> in the file counts for
 vim.opt.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent
