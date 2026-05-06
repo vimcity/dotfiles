dcf77ed30aed8a293c67d7b134aad611802ed807
vimcity <ritvikgaur7@gmail.com>
2026-05-05 19:44:37 -0700

    oil

--- nvim/lua/plugins/dap-ui.lua
+++ nvim/lua/plugins/dap-ui.lua
@@ -2,6 +2,23 @@ return {
   {
     "rcarriga/nvim-dap-ui",
     cmd = { "DapContinue", "DapUiToggle" },
+    config = function(_, opts)
+      local dap = require("dap")
+      local dapui = require("dapui")
+
+      dapui.setup(opts)
+      dap.defaults.fallback.terminal_win_cmd = "tabnew"
+
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open({})
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close({})
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close({})
+      end
+    end,
     opts = {
       force_close = false, -- Keeps the DAP UI open after debug ends
     },
@@ -11,4 +28,3 @@ return {
     cmd = { "DapContinue" },
   },
 }
-
