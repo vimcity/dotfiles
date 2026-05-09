9e7b1e799e2e9c33dfcab9a30963a888a53d0299
ritvik7 <ritvikgaur7@gmail.com>
2026-05-09 10:09:55 -0700

    remove sessions custom

--- tmux/scripts/sesh-move-pane.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-target=$(tmux list-windows -a -F '#{session_name}:#{window_index} #{window_name}' | fzf-tmux -p --prompt="Move pane to: " | awk '{print $1}')
-[ -n "$target" ] && tmux join-pane -t "$target"
