a9ac5a76fbc184ab06a348a3063f377714b8003c
vimcity <ritvikgaur7@gmail.com>
2026-05-20 11:09:00 -0700

    org tmux popup, nvim aliases

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -12,38 +12,38 @@ vim.keymap.set("n", "<leader>e", function()
 end, { desc = "Open file explorer" })
 
 -- Project management
--- vim.keymap.set("n", "<leader>fp", function()
--- Expand the path before passing to the async finder
--- local projects_dir = vim.fn.expand("~/Projects")
--- local dotfiles_dir = vim.fn.expand("~/dotfiles")
+vim.keymap.set("n", "<leader>fp", function()
+  -- Expand the path before passing to the async finder
+  local projects_dir = vim.fn.expand("~/Projects")
+  local dotfiles_dir = vim.fn.expand("~/dotfiles")
 
---   Snacks.picker.projects({
---     dev = { projects_dir, dotfiles_dir },
---     recent = false, -- Show all projects, not just recently visited
---     -- Simple finder: just list all directories in Projects
---     finder = function(opts, ctx)
---       local dev_dirs = type(opts.dev) == "string" and { opts.dev } or opts.dev or {}
---
---       return function(cb)
---         for _, dev_dir in ipairs(dev_dirs) do
---           local handle = vim.loop.fs_scandir(dev_dir)
---           if handle then
---             while true do
---               local name, type_name = vim.loop.fs_scandir_next(handle)
---               if not name then
---                 break
---               end
---               if type_name == "directory" then
---                 local project_path = dev_dir .. "/" .. name
---                 cb({ file = project_path, text = name, dir = true })
---               end
---             end
---           end
---         end
---       end
---     end,
---   })
--- end, { desc = "Find Projects" })
+  Snacks.picker.projects({
+    dev = { projects_dir, dotfiles_dir },
+    recent = false, -- Show all projects, not just recently visited
+    -- Simple finder: just list all directories in Projects
+    finder = function(opts, ctx)
+      local dev_dirs = type(opts.dev) == "string" and { opts.dev } or opts.dev or {}
+
+      return function(cb)
+        for _, dev_dir in ipairs(dev_dirs) do
+          local handle = vim.loop.fs_scandir(dev_dir)
+          if handle then
+            while true do
+              local name, type_name = vim.loop.fs_scandir_next(handle)
+              if not name then
+                break
+              end
+              if type_name == "directory" then
+                local project_path = dev_dir .. "/" .. name
+                cb({ file = project_path, text = name, dir = true })
+              end
+            end
+          end
+        end
+      end
+    end,
+  })
+end, { desc = "Find Projects" })
 
 -- Copy buffer path to clipboard
 vim.keymap.set("n", "<leader>yp", function()
@@ -51,6 +51,10 @@ vim.keymap.set("n", "<leader>yp", function()
   vim.fn.setreg("+", path)
   vim.notify("Copied: " .. path, vim.log.levels.INFO)
 end, { desc = "Copy buffer path" })
+
+vim.keymap.set("n", "<leader>fr", function()
+  Snacks.picker.recent()
+end, { desc = "Recent Files" })
 
 -- Resize windows with Ctrl + Arrow keys (10 rows/columns per press)
 vim.keymap.set("n", "<C-Up>", "<cmd>resize +10<cr>", { desc = "Increase window height" })
