4fba654dc06c85b20d6ec14e5e1f89938ce85006
rgaur <ritvik.gaur@sony.com>
2026-06-04 11:41:35 -0700

    clean

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,15 +1,8 @@
 # ruff: noqa: F821, F822
 # pyright: reportUndefinedVariable=false, reportGeneralTypeIssues=false
 
-from pathlib import Path
-
-
-def source(filename: str) -> None:
-    path = Path(__file__).with_name(filename)
-    if path.exists():
-        exec(compile(path.read_text(), str(path), "exec"), globals())
-
+import os
 
 config.load_autoconfig()
-source("main.py")
-source("local.py")
+config.source(os.path.join(os.path.dirname(__file__), "main.py"))
+config.source(os.path.join(os.path.dirname(__file__), "local.py"))
