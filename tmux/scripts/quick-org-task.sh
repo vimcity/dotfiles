87f076b64b41da925672cde140c86f05e9dd8ccc
rgaur <ritvik.gaur@sony.com>
2026-07-28 16:49:45 -0700

    org stuff

--- tmux/scripts/quick-org-task.sh
+++ tmux/scripts/quick-org-task.sh
@@ -2,4 +2,7 @@
 
 set -euo pipefail
 
+org_path="${ORG_PATH:-$HOME/Documents/org}"
+cd "$org_path"
+
 exec env NVIM_ORG_POPUP=1 nvim '+lua require("orgmode").action("capture.open_template_by_shortcut", "t")'
