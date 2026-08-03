fd76413b51b71f801e267c407e4e26bcad769e67
vimcity <ritvikgaur7@gmail.com>
2026-08-03 11:16:24 -0700

    pi refine

--- zshrc.d/fence.zsh
+++ zshrc.d/fence.zsh
@@ -3,9 +3,9 @@
 # Blocks destructive commands, credential reads, and config writes.
 # See ~/.config/fence/fence.jsonc for the full policy.
 
-# alias pi='fence -- pi'                       # Default: fence-wrapped Pi
-# alias pi-read='fence --block-net -- pi'      # No network (untrusted projects)
-# alias pi-yolo='pi'                           # pi is fenced by default; for true yolo: command -v pi
+alias pi='fence -- pi'                       # Default: Fence-wrapped Pi
+alias pi-read='fence --block-net -- pi'      # No network (untrusted projects)
+alias pi-yolo='command pi'                    # Explicitly bypass Fence
 
 # Work agent aliases (uncomment on work machine)
 # alias codex='fence -- codex'
