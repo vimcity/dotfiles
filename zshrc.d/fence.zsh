9dc3764e4ebd4831f8c176ef772782513238238a
vimcity <ritvikgaur7@gmail.com>
2026-08-04 00:48:07 -0700

    qute discard

--- zshrc.d/fence.zsh
+++ zshrc.d/fence.zsh
@@ -3,9 +3,9 @@
 # Blocks destructive commands, credential reads, and config writes.
 # See ~/.config/fence/fence.jsonc for the full policy.
 
-alias pi='fence -- pi'                       # Default: Fence-wrapped Pi
-alias pi-read='fence --block-net -- pi'      # No network (untrusted projects)
-alias pi-yolo='command pi'                    # Explicitly bypass Fence
+# alias pi='fence -- pi'                       # Default: Fence-wrapped Pi
+# alias pi-read='fence --block-net -- pi'      # No network (untrusted projects)
+# alias pi-yolo='command pi'                    # Explicitly bypass Fence
 
 # Work agent aliases (uncomment on work machine)
 # alias codex='fence -- codex'
